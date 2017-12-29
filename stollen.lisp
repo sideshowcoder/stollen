3f3a3efbcaa576449658d10d6091ea36d4904e65
Philipp Fehre <philipp@fehre.co.uk>
2017-12-29 13:03:26 +0000

    cleanup

--- stollen.lisp
+++ stollen.lisp
@@ -1,22 +1,7 @@
 (in-package #:stollen)
 
-;; todo optimize as this takes ages
-;; todo don't write the png every loop but just once at the end
-;; todo make this colorful
-;; todo make the macro a normal function calling a render-pixel function for each pixel coordinate
-;; todo make this multicore
-
-(defmacro with-png-data (size path data &body body)
-  (let ((png (gensym)))
-    `(let* ((,png (make-instance 'png
-                                 :color-type :grayscale-alpha
-                                 :width ,size
-                                 :height ,size))
-            (,data (data-array ,png)))
-       (progn ,@body (write-png ,png ,path)))))
-
-(defun calculate-mandelbrot (x y max)
-  (let ((c (complex (- (/ x 100.0) 1.5) (- (/ y 100.0) 1.5)))
+(defun calculate-mandelbrot (x y max &key transpose-x transpose-y scale)
+  (let ((c (complex (- (/ x (* 100.0 scale)) transpose-x) (- (/ y (* 100.0 scale)) transpose-y)))
         (z (complex 0.0 0.0))
         (i 0))
     (loop
@@ -27,9 +12,21 @@
              ((= i max)
               (return 255))))))
 
-(defun draw-mandelbrot (path)
-  (let ((size 300))
-    (with-png-data size path image
-      (loop for x from 0 to (- size 1)
-         do (loop for y from 0 to (- size 1)
-               do (setf (aref image x y 1) (calculate-mandelbrot x y 100)))))))
+(defun mandelbrot-png (w h &key transpose-x transpose-y scale)
+  (let* ((png (make-instance 'png
+                             :color-type :grayscale-alpha
+                             :width w
+                             :height h))
+         (data (data-array png)))
+    (loop for x from 0 to (- h 1)
+       do (loop for y from 0 to (- w 1)
+             do (setf (aref data x y 1) (calculate-mandelbrot x y 255
+                                                              :transpose-y transpose-y
+                                                              :transpose-x transpose-x
+                                                              :scale scale))))
+    png))
+
+;; Generate some images
+(write-png
+ (mandelbrot-png 1500 1000 :transpose-x 1.0 :transpose-y 1.0 :scale 10)
+ "~/Desktop/test.png")
